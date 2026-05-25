e53cb493e8ced8eab4f41189a00b8165558780e4
Daniela Belén Chesko <chesko-estudios@hotmail.com>
2026-05-25 19:26:03 -0300

    acreditar y debitar en los tipos de cuenta

--- src/ar/edu/ungs/billetera/CuentaPremium.java
+++ src/ar/edu/ungs/billetera/CuentaPremium.java
@@ -3,29 +3,26 @@ package ar.edu.ungs.billetera;
 public class CuentaPremium extends Cuenta{
 	private final double MONTO_MINIMO = 500000;
 	
-	public CuentaPremium(double montoInicial,String CVU, String alias, String DNIPropietario) {
+	public CuentaPremium(String CVU, String alias, String DNIPropietario) {
 		super(CVU, alias, DNIPropietario);	
-		
-	if (montoInicial<MONTO_MINIMO) {
-		throw new IllegalArgumentException("Monto inicial debe ser superior al monto minimo");
 	}
-	
-	establecerSaldoInicial(montoInicial);
-	}
-
-	public void debitar(boolean monto) {
-		
-	}
-	
-	public void validarReglas() {
-		
-	}
-
 
 	@Override
+	public boolean puedeAcreditar(double monto) {
+		return true;
+	}
+
+	@Override
+	public boolean puedeDebitar(double monto) {
+		return (this.saldo - monto) >= MONTO_MINIMO;
+	}
+	
+	@Override
 	public String toString() {
+		StringBuilder sb = new StringBuilder();
 		
+		sb.append("Premium: ").append(this.alias).append("(").append(this.CVU).append(")");
 		
-		return null;
+		return sb.toString();
 	}
 }
