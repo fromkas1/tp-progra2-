543c29a99166b26f59217db754f2e3dbb7c318e9
Daniela Belén Chesko <77978678+DanielaChesko@users.noreply.github.com>
2026-05-26 16:39:34 -0300

    creacion de variable para que el numeroOperacion vaya incrementandose en cada actividad

--- src/ar/edu/ungs/billetera/Actividad.java
+++ src/ar/edu/ungs/billetera/Actividad.java
@@ -3,14 +3,17 @@ package ar.edu.ungs.billetera;
 import java.time.LocalDate;
 
 public abstract class Actividad {
+	private static int contadorActividades = 1; //para que el numeroOperacion se vaya incrementando en cada actividad
 	protected String numeroOperacion;
 	protected LocalDate fecha;
 	protected double monto;
 	
-	public Actividad(String numeroOperacion, double monto) {
+	public Actividad(double monto) {
 		this.fecha = Utilitarios.hoy();
-		this.numeroOperacion = numeroOperacion;
 		this.monto = monto;
+		this.numeroOperacion = "Operacion N°" + contadorActividades;
+
+		contadorActividades++;
 	}
 	
 	public abstract String toString();
